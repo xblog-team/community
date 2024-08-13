4795c5993d261c94992eb3b311944859d81133e6
SeinJs <drasein@gmail.com>
2024-08-13 18:36:08 +0900

    init

---
+++ src/main/java/com/xblog/post_community/PostCommunityApplication.java
@@ -0,0 +1,13 @@
+package com.xblog.post_community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostCommunityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostCommunityApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/xblog/post_community/PostCommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xblog.post_community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostCommunityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
