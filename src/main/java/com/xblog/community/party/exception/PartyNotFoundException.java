2d1073c162c614584833d90df7f95b56bf4a1875
badgelatte <saucy5939@gmail.com>
2024-08-19 22:58:01 +0900

    feat: 찾고자하는 그룹이 없는 경우의 exception
    자신이 원하는 메세지를 넣어서 출력한다

---
+++ src/main/java/com/xblog/community/party/exception/PartyNotFoundException.java
@@ -0,0 +1,7 @@
+package com.xblog.community.party.exception;
+
+public class PartyNotFoundException extends RuntimeException {
+    public PartyNotFoundException(String message) {
+        super(message);
+    }
+}
