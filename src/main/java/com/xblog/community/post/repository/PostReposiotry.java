439315a546c1f952e933c3421c911d74cfa24380
badgelatte <saucy5939@gmail.com>
2024-08-20 17:34:26 +0900

    feat:특정 카테고리 아이디에 해당하는 게시물 모두 가져오는 method 추가
    categoryId에 해당하는 모든 Post를 반환하는 메소드 추가

--- src/main/java/com/xblog/community/post/repository/PostReposiotry.java
+++ src/main/java/com/xblog/community/post/repository/PostReposiotry.java
@@ -3,5 +3,8 @@ package com.xblog.community.post.repository;
 import com.xblog.community.post.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface PostReposiotry extends JpaRepository<Post, Long> {
+    List<Post> findByCategory_CategoryId(Long categoryId);
 }
