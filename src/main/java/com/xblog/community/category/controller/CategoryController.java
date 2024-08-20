9bbbf83d2d2608d46b6f424f0e55b075360036be
badgelatte <saucy5939@gmail.com>
2024-08-20 18:09:12 +0900

    feat: CategoryController 작성
    카테고리를 추가하는 메소드(addCategory), 특정 카테고리 추가하는 메소드(getCategory), 특정 그룹에 속한 모든 카테고리 조회하는 메소드(getCategoryByPartyId), 특정 카테고리 수정하는 메소드(putCategory) 추가

---
+++ src/main/java/com/xblog/community/category/controller/CategoryController.java
@@ -0,0 +1,44 @@
+package com.xblog.community.category.controller;
+
+import com.xblog.community.category.dto.AddCategoryRequest;
+import com.xblog.community.category.dto.GetCategoryResponse;
+import com.xblog.community.category.dto.ModifyCategoryDto;
+import com.xblog.community.category.service.CategoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/category")
+@RestController
+@RequiredArgsConstructor
+public class CategoryController {
+    private final CategoryService categoryService;
+
+    @PostMapping
+    public ResponseEntity<Void> addCategory(@RequestBody AddCategoryRequest dto) {
+        categoryService.createCategory(dto);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<GetCategoryResponse> getCategory(@PathVariable long categoryId) {
+        GetCategoryResponse body = categoryService.viewCaegory(categoryId);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+
+    @GetMapping("/{partyId}")
+    public ResponseEntity<List<GetCategoryResponse>> getCategoryByPartyId(@PathVariable long partyId) {
+        List<GetCategoryResponse> list = categoryService.getCategoryList(partyId);
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+    @PutMapping("/{categoryId}")
+    public ResponseEntity<ModifyCategoryDto> putCategory(@RequestBody ModifyCategoryDto dto, @PathVariable long categoryId) {
+        ModifyCategoryDto body = categoryService.modifyCategory(dto, categoryId);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+}
+
