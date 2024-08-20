e182604175e37fffc68c9a288b33702d10bcf1a4
badgelatte <saucy5939@gmail.com>
2024-08-20 17:32:00 +0900

    feat: postController 작성
    게시물 추가하는 메소드(addPost), 특정 게시물 조회하는 메소드(getPost), 특정 카테고리 속 게시물을 조회하는 메소드(getPostByCategory), 특정 게시물 조회하는 메소드(putPost), 특정 게시물 삭제하는 메소드(deletePost)를 구현

---
+++ src/main/java/com/xblog/community/post/controller/PostController.java
@@ -0,0 +1,51 @@
+package com.xblog.community.post.controller;
+
+import com.xblog.community.post.dto.AddPostDto;
+import com.xblog.community.post.dto.GetPostResponse;
+import com.xblog.community.post.dto.ModifyPostRequeset;
+import com.xblog.community.post.dto.ModifyPostResponse;
+import com.xblog.community.post.service.PostService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RequestMapping("/api/post")
+@RestController
+@RequiredArgsConstructor
+public class PostController {
+    private final PostService postService;
+
+    @PostMapping
+    public ResponseEntity<AddPostDto> addPost(@RequestBody AddPostDto dto, @RequestHeader("X-User-Id") String userId) {
+        AddPostDto body = postService.createPost(dto, userId);
+        return ResponseEntity.status(HttpStatus.CREATED).body(body);
+    }
+
+    @GetMapping("/{postId}")
+    public ResponseEntity<GetPostResponse> getPost(@PathVariable Long postId){
+        GetPostResponse dto = postService.viewPost(postId);
+        return ResponseEntity.status(HttpStatus.OK).body(dto);
+    }
+
+    @GetMapping("/{categoryId}")
+    public ResponseEntity<List<GetPostResponse>> getPostByCategory(@PathVariable Long categoryId){
+        List<GetPostResponse> list = postService.getPostList(categoryId);
+        return ResponseEntity.status(HttpStatus.OK).body(list);
+    }
+
+    @PutMapping("/{postId}")
+    public ResponseEntity<ModifyPostResponse> putPost(@RequestBody ModifyPostRequeset dto, @PathVariable Long postId, @RequestHeader("X-User-Id") String userId) {
+        ModifyPostResponse body = postService.modifyPost(dto, postId, userId);
+        return ResponseEntity.status(HttpStatus.OK).body(body);
+    }
+
+    @DeleteMapping("/{postId}")
+    public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
+        postService.deletePost(postId);
+        return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+}
