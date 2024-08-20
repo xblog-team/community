cfbf74c47a1bf430cfd227942fb3b83821c4e2c9
badgelatte <saucy5939@gmail.com>
2024-08-20 18:02:56 +0900

    feat: 특정 그룹 아이디에서 사용하는 카테고리를 모두 가져오는 method 추가
    partyId를 가진 모든 category 반환하는 메소드 추가

--- src/main/java/com/xblog/community/category/repository/CategoryRepository.java
+++ src/main/java/com/xblog/community/category/repository/CategoryRepository.java
@@ -3,5 +3,8 @@ package com.xblog.community.category.repository;
 import com.xblog.community.category.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByParty_PartyId(Long partyId);
 }
