e66ac94e3eca3e9dc116236d4a9cfe45501f810b
badgelatte <saucy5939@gmail.com>
2024-08-28 19:24:15 +0900

    fix: api 규칙에 맞게 수정
    하이폰 사용, 행위 포함하지 않기, 영어를 복수형으로 작성

--- src/main/java/com/xblog/community/post/controller/PostController.java
+++ src/main/java/com/xblog/community/post/controller/PostController.java
@@ -12,37 +12,37 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RequestMapping("/api")
+@RequestMapping("/api/posts")
 @RestController
 @RequiredArgsConstructor
 public class PostController {
     private final PostService postService;
 
-    @PostMapping("/post")
+    @PostMapping
     public ResponseEntity<AddPostDto> addPost(@RequestBody AddPostDto dto, @RequestHeader("X-User-Id") String userId) {
         AddPostDto body = postService.createPost(dto, userId);
         return ResponseEntity.status(HttpStatus.CREATED).body(body);
     }
 
-    @GetMapping("/post/{postId}")
+    @GetMapping("/{postId}")
     public ResponseEntity<GetPostResponse> getPost(@PathVariable Long postId){
         GetPostResponse dto = postService.viewPost(postId);
         return ResponseEntity.status(HttpStatus.OK).body(dto);
     }
 
-    @GetMapping("/posts/{categoryId}")
+    @GetMapping("/post-categories/{categoryId}")
     public ResponseEntity<List<GetPostResponse>> getPostByCategory(@PathVariable Long categoryId){
         List<GetPostResponse> list = postService.getPostList(categoryId);
         return ResponseEntity.status(HttpStatus.OK).body(list);
     }
 
-    @PutMapping("/post/{postId}")
+    @PutMapping("/{postId}")
     public ResponseEntity<ModifyPostResponse> putPost(@RequestBody ModifyPostRequeset dto, @PathVariable Long postId, @RequestHeader("X-User-Id") String userId) {
         ModifyPostResponse body = postService.modifyPost(dto, postId, userId);
         return ResponseEntity.status(HttpStatus.OK).body(body);
     }
 
-    @DeleteMapping("/post/{postId}")
+    @DeleteMapping("/{postId}")
     public ResponseEntity<Void> deletePost(@PathVariable Long postId) {
         postService.deletePost(postId);
         return ResponseEntity.status(HttpStatus.OK).build();
