a32b22bcb2253ac8d792fe2abd7cd45fac5558b9
SeinJs <drasein@gmail.com>
2024-09-02 22:09:14 +0900

    Refactor: user party enum 변수 대문자로 수정

--- src/main/java/com/xblog/community/user_party/controller/UserPartyController.java
+++ src/main/java/com/xblog/community/user_party/controller/UserPartyController.java
@@ -20,8 +20,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -58,7 +56,7 @@ public class UserPartyController {
     }
 
     @PutMapping("/{partyId}/approve")
-    public ResponseEntity<Void> approveUser(@PathVariable("partyId") Long partyId, @RequestHeader("X-USER-ID") String userId) {
+    public ResponseEntity<Void> approveUser(@PathVariable("partyId") Long partyId, @RequestParam String userId) {
         userPartyService.approveUser(userId, partyId);
         return ResponseEntity.ok().build();
     }
