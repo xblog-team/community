887797c6968a8e4cff2f063dd0efef3a1d6f62f6
SeinJs <drasein@gmail.com>
2024-09-27 20:32:36 +0900

    Refactor: 필요없는 annotation 제거

--- src/main/java/com/xblog/community/user/dto/LoginInfoResponseDto.java
+++ src/main/java/com/xblog/community/user/dto/LoginInfoResponseDto.java
@@ -4,12 +4,8 @@ import java.util.List;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
-@NoArgsConstructor
 @AllArgsConstructor
 public class LoginInfoResponseDto {
     private String userId;
